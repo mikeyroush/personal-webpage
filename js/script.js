d3645b0e100157aad193afe3e4d77aa7ff19241d
mikeyroush <mikey.roush97@yahoo.com>
2019-12-24 18:03:56 -0600

    added active slideshow buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
 const navToggle = document.querySelector(".nav__toggle");
 const nav = document.querySelector(".nav");
 const home = document.querySelector("#home");
+const i1 = document.querySelector(".i1");
+const i2 = document.querySelector(".i2");
+const i3 = document.querySelector(".i3");
+const i4 = document.querySelector(".i4");
+const b1 = document.querySelector(".bar1");
+const b2 = document.querySelector(".bar2");
+const b3 = document.querySelector(".bar3");
+const b4 = document.querySelector(".bar4");
+const thresh = 0.8;
 
 if (navToggle)
   navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-open");
   });
 
-const headerObserverOptions = { threshold: 0.8 };
+//toggle navbar background
+const headerObserverOptions = {
+  threshold: thresh
+};
 
 const headerObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if (!entry.isIntersecting) {
+    console.log(entry);
+    if (entry.intersectionRatio < thresh) {
       nav.classList.add("nav-scrolled");
     } else {
       nav.classList.remove("nav-scrolled");
@@ -20,3 +33,68 @@ const headerObserver = new IntersectionObserver(entries => {
 }, headerObserverOptions);
 
 headerObserver.observe(home);
+
+//toggle active slideshow button
+const i1ObserverOptions = {
+  threshold: thresh
+};
+
+const i1Observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > thresh) {
+      b1.classList.add("active");
+    } else {
+      b1.classList.remove("active");
+    }
+  });
+}, i1ObserverOptions);
+
+i1Observer.observe(i1);
+
+const i2ObserverOptions = {
+  threshold: thresh
+};
+
+const i2Observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > thresh) {
+      b2.classList.add("active");
+    } else {
+      b2.classList.remove("active");
+    }
+  });
+}, i2ObserverOptions);
+
+i2Observer.observe(i2);
+
+const i3ObserverOptions = {
+  threshold: thresh
+};
+
+const i3Observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > thresh) {
+      b3.classList.add("active");
+    } else {
+      b3.classList.remove("active");
+    }
+  });
+}, i3ObserverOptions);
+
+i3Observer.observe(i3);
+
+const i4ObserverOptions = {
+  threshold: thresh
+};
+
+const i4Observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > thresh) {
+      b4.classList.add("active");
+    } else {
+      b4.classList.remove("active");
+    }
+  });
+}, i4ObserverOptions);
+
+i4Observer.observe(i4);
